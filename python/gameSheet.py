15af3bf1d3c7e0e2e53bca703e43b98ed83549e7
davef77 <davefarley77@gmail.com>
2015-04-03 17:07:56 +0100

    Initial Commit of Battleships game - Story 1

--- python/gameSheet.py
+++ python/gameSheet.py
@@ -1,25 +1,14 @@
-from python.orientation import Horizontal
-
 MAX_COLUMNS = 8
-MAX_ROWS = 8
 
 ROW_NAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 
 class GameSheet:
-
     def __init__(self):
         self.sheet = {}
-        self.width = MAX_COLUMNS
-        self.height = MAX_ROWS
 
         for row in ROW_NAMES:
             self.__init_row(row)
-
-    def place_ship(self, ship):
-        if ship.orientation is Horizontal:
-            for column in range(ship.column, ship.column + ship.waterline_length):
-                self._set_cell_value(ship.row, column - 1, ship.id)
 
     def cell_value(self, row, column):
         return self.sheet[row][column]
@@ -57,6 +46,3 @@ class GameSheet:
 
         for column in range(0, MAX_COLUMNS):
             self.sheet[row].append('.')
-
-    def _set_cell_value(self, row, column, value):
-        self.sheet[row][column] = value
--- python/tests/test_gameSheet.py
+++ python/tests/test_gameSheet.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 from python.gameSheet import GameSheet
-from python.orientation import Horizontal
-from python.ships import AircraftCarrier
 
 EMPTY_GAME_SHEET = "A........\nB........\nC........\nD........\nE........\nF........\nG........\nH........\n.12345678"""
 
@@ -13,8 +11,3 @@ class GameSheetTest(TestCase):
 
     def test_should_render_as_cartesian_plane(self):
         self.assertEqual(EMPTY_GAME_SHEET, str(self.game))
-
-    def test_should_place_an_aircraft_carrier_horizontally(self):
-        self.game.place_ship(AircraftCarrier(self.game, 'A1', Horizontal))
-
-        self.assertEquals("AAAAAA...", str(self.game)[:9])
